ed440be5af421ac7cfa1e8dab10d772ef2fbf6d4
Leonardo Valverde <leovmsi@hotmail.com>
2024-06-07 17:23:16 -0300

    feat: create ColoredDivider component

---
+++ src/components/ColoredDivider.vue
@@ -0,0 +1,25 @@
+<template>
+  <div :style="dividerStyle"></div>
+</template>
+
+<script>
+export default {
+  name: "ColoredDivider",
+  props: {
+    color: {
+      type: String,
+      default: "#000",
+    },
+  },
+  computed: {
+    dividerStyle() {
+      return {
+        height: "6px",
+        backgroundColor: this.color,
+        margin: "48px 0",
+        borderRadius: "25px",
+      };
+    },
+  },
+};
+</script>
