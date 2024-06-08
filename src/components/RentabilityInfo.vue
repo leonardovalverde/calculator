398d4051be05392f3558eaecb21e89bc225a25a0
Leonardo Valverde <leovmsi@hotmail.com>
2024-06-08 16:04:51 -0300

    feat: improving RentabilityInfo component

---
+++ src/components/RentabilityInfo.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="info-box">
+    <div class="rentability-container">
+      <div class="rentability">
+        <span> Taxa Selic: </span>
+        <strong>{{ taxaSelic }}</strong>
+      </div>
+      <div>
+        <span> Rentabilidade do Arca: </span>
+        <strong>{{ rentabilidadeArca }}</strong>
+      </div>
+    </div>
+    <div class="info-text-container">
+      <img src="@/assets/svg/help.svg" alt="ícone de ajuda" class="icon" />
+      <div>
+        <p class="date-info-text">
+          Valores utilizados no simulador de investimentos (referentes à data de
+          última atualização - esses valores podem alterar de acordo com o
+          mercado):
+        </p>
+        <p>- Data da última atualização: {{ dataAtualizacao }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    taxaSelic: String,
+    rentabilidadeArca: String,
+    dataAtualizacao: String,
+  },
+};
+</script>
+
+<style scoped>
+.info-box {
+  strong {
+    font-family: var(--font-family-tertiary);
+    color: black;
+    white-space: nowrap;
+    font-size: 18px;
+    font-weight: 800;
+    text-transform: initial;
+  }
+}
+
+.rentability-container {
+  span {
+    text-transform: uppercase;
+    font-weight: 600;
+    letter-spacing: 2px;
+    color: var(--font-color);
+  }
+}
+
+.rentability {
+  margin-bottom: 32px;
+}
+
+.info-text-container {
+  font-family: var(--font-family-main);
+  display: flex;
+  margin:49px 0 24px 0;
+  align-items: start;
+}
+
+.date-info-text {
+  margin-bottom: 24px;
+}
+
+.icon {
+  margin-right: 16px;
+}
+</style>
